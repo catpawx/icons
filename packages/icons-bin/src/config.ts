51ba838ff38b856244f1f900a26fe09a8b167186
相甫 <xiangfu_git@163.com>
2025-01-06 22:39:49 +0800

    feat(icons-bin): 修改脚本!

--- packages/icons-bin/src/config.ts
+++ packages/icons-bin/src/config.ts
@@ -1,12 +1,16 @@
 // 路径参数
-export const pathList = ['seres-react-icon']
+export const pathList = ['icon-react']
 
 // 默认的图标样式
 export const STYLESKEY = {
   OUTLINED: 'outlined',
+  FILLED: 'filled',
   TWOTONE: 'twotone',
   MULTITONE: 'multitone',
 }
 
 // 默认的图标样式
 export const defaultStyle = STYLESKEY.OUTLINED
+
+// 临时目录
+export const TEMP_DIR = '.icons'
--- packages/icons-bin/src/generate-icons.ts
+++ packages/icons-bin/src/generate-icons.ts
@@ -1,11 +1,8 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import fs from 'fs'
 import path from 'path'
 
-// @ts-ignore
 // import format from 'prettier-eslint'
-// import iconsData from '../.icons/data.json'
-import { defaultStyle } from './config'
+import { defaultStyle, TEMP_DIR } from './config'
 import processSvg from './process-svg'
 import { getAttrs, getElementCode, getStyles, getTypes } from './template'
 import { copy, parseName } from './utils'
@@ -23,16 +20,21 @@ interface Icon {
 
 const rootDir = path.resolve()
 
-// where icons code in
-const srcDir = path.join(rootDir, '.icons')
-const iconsDir = path.join(rootDir, '.icons/icons')
-const iconsIndexFile = path.join(rootDir, '.icons', 'index.ts')
-const iconsJsonFile = path.join(rootDir, '.icons/data.json')
+/** 整个操作的临时目录 */
+const tempDir = path.join(rootDir, TEMP_DIR)
+/** 图标组件目录 */
+const iconsDir = path.join(tempDir, 'icons')
+/** svg 目录 */
+const svgsDir = path.join(tempDir, 'svg')
+/** 生成的 index.ts 文件 */
+const iconsIndexFile = path.join(tempDir, 'index.ts')
+/** 生成的 data.json 文件 */
+const iconsJsonFile = path.join(tempDir, 'data.json')
 
 // generate icons.js and icons.d.ts file
 const generateIconsIndex = () => {
-  if (!fs.existsSync(srcDir)) {
-    fs.mkdirSync(srcDir)
+  if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir)
     fs.mkdirSync(iconsDir)
   } else if (!fs.existsSync(iconsDir)) {
     fs.mkdirSync(iconsDir)
@@ -66,8 +68,8 @@ const attrsToString = (attrs: { [key: string]: string }) => {
 const generateIconCode = async ({ name }: { name: any }) => {
   const names = parseName(name, defaultStyle)
   console.log(names)
-  const location = path.join(rootDir, '.icons/svg', `${names.name}.svg`)
-  const destination = path.join(rootDir, '.icons/icons', `${names.name}.tsx`)
+  const location = path.join(svgsDir, `${names.name}.svg`)
+  const destination = path.join(iconsDir, `${names.name}.tsx`)
   const code: any = fs.readFileSync(location)
   const svgCode = await processSvg(code, names)
   const ComponentName = names.componentName
@@ -107,18 +109,31 @@ const appendToIconsIndex = ({
   fs.appendFileSync(iconsIndexFile, exportString, 'utf-8')
 }
 
+/** 加载本地的json数据 */
+export const loadLocalJson = () => {
+  return JSON.parse(fs.readFileSync(iconsJsonFile, 'utf-8'))
+}
+
+/** 将资源复制到指定目录 */
+export const copyIconsToOutput = () => {
+  const { OUTPUT_DIR } = process.env
+  // 将 styles.ts 和types.ts 复制到 输出目录下
+  const outputDir = path.join(rootDir, OUTPUT_DIR || '')
+  const outputiconsIndexFile = path.join(outputDir, 'index.ts')
+  const outputStylesFile = path.join(outputDir, 'styles.ts')
+  const outputTypesFile = path.join(outputDir, 'types.ts')
+  copy(iconsDir, path.join(outputDir, 'icons'))
+  copy(iconsIndexFile, outputiconsIndexFile)
+  fs.writeFileSync(outputStylesFile, getStyles(), 'utf-8')
+  fs.writeFileSync(outputTypesFile, getTypes(), 'utf-8')
+  // 删除临时目录
+  fs.rmSync(tempDir, { recursive: true })
+}
+
 /** 生成图标 */
 export const generateIcons = () => {
   const icons: { [key: string]: Icon } = loadLocalJson()
   generateIconsIndex()
-
-  // Object.keys(icons)
-  // .map(key => icons[key])
-  // .forEach(({ name }) => {
-  //   generateIconCode({ name }).then(({ ComponentName, name }) => {
-  //     appendToIconsIndex({ ComponentName, name })
-  //   })
-  // })
 
   const tasks = Object.keys(icons)
     .map(key => icons[key])
@@ -132,25 +147,4 @@ export const generateIcons = () => {
     copyIconsToOutput()
   })
   // 将上面的异步任务执行后，将 icons 复制到指定目录
-}
-
-/** 加载本地的json数据 */
-export const loadLocalJson = () => {
-  return JSON.parse(fs.readFileSync(iconsJsonFile, 'utf-8'))
-}
-
-/** 将资源复制到指定目录 */
-export const copyIconsToOutput = () => {
-  const { OUTPUT_DIR } = process.env
-  // 将 styles.ts 和types.ts 复制到 .icons 目录下
-  const outputDir = path.join(rootDir, OUTPUT_DIR || '')
-  const outputiconsIndexFile = path.join(outputDir, 'index.ts')
-  const outputStylesFile = path.join(outputDir, 'styles.ts')
-  const outputTypesFile = path.join(outputDir, 'types.ts')
-  copy(iconsDir, path.join(outputDir, 'icons'))
-  copy(iconsIndexFile, outputiconsIndexFile)
-  fs.writeFileSync(outputStylesFile, getStyles(), 'utf-8')
-  fs.writeFileSync(outputTypesFile, getTypes(), 'utf-8')
-  // 删除 .icons 目录
-  fs.rmSync(srcDir, { recursive: true })
 }
